005012cf15bcb90517f8e5a404a3e068398cfe85
phaseydaisy <kaidenlorse@gmail.com>
2025-09-05 16:54:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-const CLIENT_ID = "cf5622de811e44a583bb2a888a63a63e";
-const REDIRECT_URI = "https://phawse/callback";
-const SCOPE = "user-read-currently-playing user-read-recently-played user-top-read";
+const BACKEND_AUTHORIZE_URL = "https://spotify-backend.onrender.com/authorize"
 
-document.getElementById("linkBtn").addEventListener("click", () => {
-  const discordId = prompt("Enter your Discord ID to link:");
-  if (!discordId) return alert("Discord ID required");
+document.getElementById("linkBtn").addEventListener("click", async () => {
+    const discordUserId = prompt("Enter your Discord ID");
+    if (!discordUserId) return alert("Discord ID required");
 
-  const params = new URLSearchParams({
-    client_id: CLIENT_ID,
-    response_type: "code",
-    redirect_uri: REDIRECT_URI,
-    scope: SCOPE,
-    state: discordId
-  });
-
-  window.location.href = `https://accounts.spotify.com/authorize?${params.toString()}`;
+    const url = `${BACKEND_AUTHORIZE_URL}?user_id=${discordUserId}`;
+    window.location.href = url;
 });
