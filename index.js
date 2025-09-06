8ea26175e95c83a89ec63886b3acf31c108af7db
phaseydaisy <kaidenlorse@gmail.com>
2025-09-06 02:14:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-const BACKEND_AUTHORIZE_URL = "https://spotify-backend.onrender.com/authorize"
 
-document.getElementById("linkBtn").addEventListener("click", async () => {
-    const discordUserId = prompt("Enter your Discord ID");
-    if (!discordUserId) return alert("Discord ID required");
-
-    const url = `${BACKEND_AUTHORIZE_URL}?user_id=${discordUserId}`;
-    window.location.href = url;
-});
