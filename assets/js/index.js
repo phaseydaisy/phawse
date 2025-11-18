3e6371add642420b380827805ce6dbfc01452359
phaseydaisy <kaidenlorse@gmail.com>
2025-11-18 00:03:55 -0500

    assets

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,77 @@
 const canvas = document.createElement('canvas');
 
+// Mouse Particle Trail System
+const particles = [];
+const maxParticles = 100;
+
+class Particle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.size = Math.random() * 5 + 2;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+        this.color = `hsl(${Math.random() * 60 + 240}, 100%, ${Math.random() * 30 + 60}%)`;
+        this.life = 1;
+        this.decay = Math.random() * 0.02 + 0.01;
+    }
+
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+        this.life -= this.decay;
+        this.size *= 0.98;
+    }
+}
+
+function createParticle(x, y) {
+    if (particles.length < maxParticles) {
+        particles.push(new Particle(x, y));
+    }
+}
+
+function animateParticles() {
+    const particleContainer = document.querySelector('.particle-container');
+    
+    particles.forEach((particle, index) => {
+        particle.update();
+        
+        if (particle.life <= 0) {
+            particles.splice(index, 1);
+        }
+    });
+    
+    if (particleContainer) {
+        particleContainer.innerHTML = '';
+        particles.forEach(particle => {
+            const particleEl = document.createElement('div');
+            particleEl.className = 'particle';
+            particleEl.style.left = particle.x + 'px';
+            particleEl.style.top = particle.y + 'px';
+            particleEl.style.width = particle.size + 'px';
+            particleEl.style.height = particle.size + 'px';
+            particleEl.style.background = particle.color;
+            particleEl.style.opacity = particle.life;
+            particleContainer.appendChild(particleEl);
+        });
+    }
+    
+    requestAnimationFrame(animateParticles);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const particleContainer = document.createElement('div');
+    particleContainer.className = 'particle-container';
+    document.body.appendChild(particleContainer);
+    animateParticles();
+});
+
+document.addEventListener('mousemove', (e) => {
+    for (let i = 0; i < 2; i++) {
+        createParticle(e.clientX, e.clientY);
+    }
+});
+
 document.addEventListener('click', handleLinkClick);
 
 window.addEventListener('popstate', (e) => {
